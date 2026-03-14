22b1c52402a45a8016f2b65beb93b987befba7e8
xTheWhiteLionx <hardtfabian91@gmail.com>
2026-03-14 17:45:44 +0100

    entfernen von ungenutzten imports

--- src/main/java/com/greatlearning/great_kart/GreatKartApplication.java
+++ src/main/java/com/greatlearning/great_kart/GreatKartApplication.java
@@ -1,13 +1,10 @@
 package com.greatlearning.great_kart;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.greatlearning.great_kart")
-//@EnableAutoConfiguration
+
 public class GreatKartApplication {
 
 	public static void main(String[] args) {
